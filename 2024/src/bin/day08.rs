39e3fbbcc41a445054479a430ef54bd81971708b
Nikodem Rabuliński <nikodem@rabulinski.com>
2024-12-08 15:01:41 +0100

    2024: Day 8 nicer code

--- 2024/src/bin/day08.rs
+++ 2024/src/bin/day08.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, iter::once};
 
 use aoc_lib::{
     aoc,
@@ -8,18 +8,11 @@ use aoc_lib::{
 
 static INPUT: &str = include_str!("../../inputs/day08");
 
-fn get_antinode_pos(grid: &Grid<'_>, k: Point, l: Point) -> Option<Point> {
-    let d = (k.0 - l.0, k.1 - l.1);
-    let anti = k.add(&d);
-
-    grid.is_valid_pos(anti).then_some(anti)
-}
-
-fn part1(input: &str) -> Result<usize> {
+fn parse(input: &str) -> Result<(Grid<'_>, Vec<(Point, u8)>)> {
     let input = input.trim();
     let grid = Grid::for_str(input).ok_or_eyre("invalid format")?;
 
-    let points: Vec<_> = input
+    let points = input
         .as_bytes()
         .iter()
         .enumerate()
@@ -32,75 +25,51 @@ fn part1(input: &str) -> Result<usize> {
         })
         .collect();
 
-    let mut res = HashSet::new();
+    Ok((grid, points))
+}
 
-    for i in 0..points.len() {
-        let a = points[i];
-        for j in (i + 1)..points.len() {
-            let b = points[j];
-            if a.1 != b.1 {
-                continue;
-            }
-            if let Some(pos) = get_antinode_pos(&grid, a.0, b.0) {
-                res.insert(pos);
-            }
-            if let Some(pos) = get_antinode_pos(&grid, b.0, a.0) {
-                res.insert(pos);
-            }
-        }
-    }
+fn get_antinode_pos(grid: &Grid<'_>, k: Point, l: Point) -> impl Iterator<Item = Point> {
+    std::iter::successors(Some((k, l)), |&(k, l)| {
+        let d = (k.0 - l.0, k.1 - l.1);
+        let next = k.add(&d);
+        grid.is_valid_pos(next).then_some((next, k))
+    })
+    .skip(1)
+    .map(|(pos, _)| pos)
+}
+
+fn part1(input: &str) -> Result<usize> {
+    let (grid, ref points) = parse(input)?;
+
+    let res: HashSet<_> = (0..points.len())
+        .flat_map(|i| ((i + 1)..points.len()).map(move |j| (points[i], points[j])))
+        .filter_map(|(a, b)| (a.1 == b.1).then_some((a.0, b.0)))
+        .flat_map(|(a, b)| {
+            [
+                get_antinode_pos(&grid, a, b).next(),
+                get_antinode_pos(&grid, b, a).next(),
+            ]
+            .into_iter()
+            .flatten()
+        })
+        .collect();
 
     Ok(res.len())
 }
 
 fn part2(input: &str) -> Result<usize> {
-    let input = input.trim();
-    let grid = Grid::for_str(input).ok_or_eyre("invalid format")?;
+    let (grid, ref points) = parse(input)?;
 
-    let points: Vec<_> = input
-        .as_bytes()
-        .iter()
-        .enumerate()
-        .filter_map(|(i, &c)| {
-            if c == b'.' {
-                None
-            } else {
-                grid.idx_to_pos(i).map(|pos| (pos, c))
-            }
+    let res: HashSet<_> = (0..points.len())
+        .flat_map(|i| ((i + 1)..points.len()).map(move |j| (points[i], points[j])))
+        .filter_map(|(a, b)| (a.1 == b.1).then_some((a.0, b.0)))
+        .flat_map(|(a, b)| {
+            once(a)
+                .chain(once(b))
+                .chain(get_antinode_pos(&grid, a, b))
+                .chain(get_antinode_pos(&grid, b, a))
         })
         .collect();
-
-    let mut res = HashSet::new();
-
-    for i in 0..points.len() {
-        let a = points[i];
-        for j in (i + 1)..points.len() {
-            let b = points[j];
-            if a.1 != b.1 {
-                continue;
-            }
-            res.insert(a.0);
-            res.insert(b.0);
-            let mut last = (a.0, b.0);
-            loop {
-                if let Some(pos) = get_antinode_pos(&grid, last.0, last.1) {
-                    res.insert(pos);
-                    last = (pos, last.0);
-                } else {
-                    break;
-                }
-            }
-            last = (b.0, a.0);
-            loop {
-                if let Some(pos) = get_antinode_pos(&grid, last.0, last.1) {
-                    res.insert(pos);
-                    last = (pos, last.0);
-                } else {
-                    break;
-                }
-            }
-        }
-    }
 
     Ok(res.len())
 }
