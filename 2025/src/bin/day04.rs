f3938fbd5666fc5c3c720cc3c0028dc90d25faf9
Nikodem Rabuliński <nikodem@rabulinski.com>
2025-12-04 20:34:30 +0100

    2025: Day 4 a bit nicer

--- 2025/src/bin/day04.rs
+++ 2025/src/bin/day04.rs
@@ -1,45 +1,46 @@
-use aoc_lib::{aoc, color_eyre::eyre::Result, grid::Grid};
+use aoc_lib::{
+    aoc,
+    color_eyre::eyre::Result,
+    grid::{Grid, Point},
+};
 
 static INPUT: &str = include_str!("../../inputs/day04");
 
-fn part1(input: &str) -> Result<usize> {
-    let grid = Grid::for_str(input).unwrap();
-
-    let result = (0..grid.height())
+fn reachable(grid: &Grid<'_>) -> impl Iterator<Item = Point> {
+    (0..grid.height())
         .flat_map(|y| (0..grid.width()).map(move |x| (x, y)))
         .filter(|&pos| {
             grid[pos] == b'@' && grid.adjacent_pos(pos).filter(|&p| grid[p] == b'@').count() < 4
         })
-        .count();
+}
 
+fn part1(input: &str) -> Result<usize> {
+    let grid = Grid::for_str(input).unwrap();
+    let result = reachable(&grid).count();
     Ok(result)
 }
 
 fn part2(input: &str) -> Result<usize> {
-    let mut result = 0;
-
-    let mut input = input.trim().to_string();
-    loop {
-        let grid = Grid::for_str(&input).unwrap();
-        let to_remove: Vec<_> = (0..grid.height())
-            .flat_map(|y| (0..grid.width()).map(move |x| (x, y)))
-            .filter(|&pos| {
-                grid[pos] == b'@' && grid.adjacent_pos(pos).filter(|&p| grid[p] == b'@').count() < 4
-            })
-            .map(|pos| grid.pos_to_idx(pos).unwrap())
-            .collect();
-        // no more rolls available to remove
-        if to_remove.is_empty() {
-            break;
-        }
-
-        for idx in to_remove {
-            unsafe {
-                input.as_bytes_mut()[idx] = b'.';
+    let result = std::iter::repeat(())
+        .scan(input.trim().to_string(), |input, _| {
+            let grid = Grid::for_str(&input).unwrap();
+            let to_remove: Vec<_> = reachable(&grid)
+                .map(|pos| grid.pos_to_idx(pos).unwrap())
+                .collect();
+            // no more rolls available to remove
+            if to_remove.is_empty() {
+                return None;
             }
-            result += 1;
-        }
-    }
+
+            for &idx in &to_remove {
+                unsafe {
+                    input.as_bytes_mut()[idx] = b'.';
+                }
+            }
+            Some(to_remove.len())
+        })
+        .fuse()
+        .sum();
 
     Ok(result)
 }
